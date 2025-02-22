4306efa5345879d8b71f5776f7c93e7db21581e1
Tristan Brown <2tristanbrown@gmail.com>
2025-02-22 12:51:29 -0700

    fixing some issues

--- raytracer/build/cmake_install.cmake
+++ raytracer/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/trist/Desktop/cs455/raytracer
+# Install script for directory: C:/Users/trist/Documents/GitHub/Raytracer_Practice/raytracer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/trist/Desktop/cs455/raytracer/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/trist/Documents/GitHub/Raytracer_Practice/raytracer/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/trist/Desktop/cs455/raytracer/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/trist/Documents/GitHub/Raytracer_Practice/raytracer/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- raytracer/main.cc
+++ raytracer/main.cc
@@ -1,3 +1,5 @@
+#include "color.h"
+#include "vec3.h"
 #include <iostream>
 
 int main() {
@@ -13,16 +15,19 @@ int main() {
     for (int j = 0; j < image_height; j++){
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1) ,0);
+            write_color(std::cout, pixel_color);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            // auto r = double(i) / (image_width-1);
+            // auto g = double(j) / (image_height-1);
+            // auto b = 0.0;
+
+            // int ir = int(255.999 * r);
+            // int ig = int(255.999 * g);
+            // int ib = int(255.999 * b);
 
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
     std::clog << "\rDone.                   \n";
